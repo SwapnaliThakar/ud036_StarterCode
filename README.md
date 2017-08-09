8c1cbad0b7b4d1d13d18932832075f16ef1406ab
SwapnaliThakar <swapnalit@cybersapien.ca>
2017-08-09 10:10:17 -0400

    Project completed - Movie Trailers

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Swapnali's Movie Trailers
+This project showcases my 3 favorite movies on a web page.
+
+# Getting Started
+- Clone this project locally
+- Open the file entertainment_centre.html in your browser
+
+# Built Using
+Python, HTML, Javascript
+
+# Authors
+Udacity and Swapnali Thakar
+
+# Lisence
+This is an open source project.
+
+# Acknowledgements
+Udacity's Starter Code file.
--- media.py
+++ media.py
@@ -3,14 +3,20 @@ import webbrowser
 
 class Movie():
     """This class initializes the details of a
-    movie and shows trailer as required"""
+    movie and shows trailer as required
+    
+        movie_title (str): is the title of the movie
+        movie_storyline (str): is the storyline of the movie
+        poster_image (url): is the url from which to get the poster image
+        trailer_youtube (url): is the url to the youtube trailer of the movie
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.title = movie_title  # initializes movie title
+        self.storyline = movie_storyline  # initializes storyline of the movie
+        self.poster_image_url = poster_image  # initializes poster image
+        self.trailer_youtube_url = trailer_youtube  # initializes movie trailer url
         
     # Shows trailer
     def show_trailer(self):
